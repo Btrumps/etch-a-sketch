6cd80e67e43eb09f7bc3731a44e83ca4db4682d4
Brandon Trumpold <brandon.trumpold@gmail.com>
2020-02-03 11:53:27 -0500

    added rainbow mode + status text

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const gridContainer = document.querySelector(".container");
-const button = document.querySelector("button");
+const resetButton = document.querySelector("#reset");
+const rainbowButton = document.querySelector("#rainbow");
+const statusText = document.querySelector("#status");
+let rainbowOn = false;
+
 function createGrid(width) {
     for (i = 0; i < width * width; i++) {
         var gridPiece = document.createElement('div');
         gridPiece.classList.add("gridPiece");
         gridPiece.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = "red";
+            if (rainbowOn) {
+                var randomColor = Math.floor(Math.random()*16777215).toString(16);
+                e.target.style.backgroundColor = "#" + randomColor;
+            } else {
+                e.target.style.backgroundColor = "red";
+            }
+            
         })
         gridContainer.appendChild(gridPiece);
     }
 }
 
-button.onclick = () => {
+resetButton.onclick = () => {
     gridContainer.innerHTML = "";
+    statusText.textContent = "Grid reset!"
     createGrid(16);
+;}
+
+rainbowButton.onclick = () => {
+    rainbowOn = !rainbowOn;
+    if (rainbowOn) {
+        statusText.textContent = "Rainbow Mode On!";
+    } else {
+        statusText.textContent = "Rainbow Mode Off...";
+    }
+
 ;}
 
 createGrid(16);
